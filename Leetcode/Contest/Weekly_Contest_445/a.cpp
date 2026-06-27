de888fbd7df0a5e2e67f01414d1be168b59e411d
hung146164 <nguyenbaphuchung2@gmail.com>
2026-06-28 00:12:43 +0700

    update

--- Leetcode/Contest/Weekly_Contest_445/a.cpp
+++
@@ -1,9 +0,0 @@
-class Solution {
-public:
-    int findClosest(int x, int y, int z) {
-        int a=abs(x-z);
-        int b=abs(y-z);
-        if(a==b) return 0;
-        return a<b?1:2;
-    }
-};
